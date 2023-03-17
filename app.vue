0b9d54f12ed06081f5c032258322faf73466f62b
Keisuke Kobayashi <kobakei122@gmail.com>
2023-03-17 09:45:38 +0900

    Add pinia

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <NuxtWelcome />
+    {{ store.count }}
+    <button @click="store.increment()">click</button>
   </div>
 </template>
+
+<script lang="ts">
+
+import {useCounterStore} from "~/stores/counter";
+
+export default {
+  data() {
+    return {
+      store: useCounterStore()
+    }
+  },
+}
+</script>
